c4c51b780032c42d2bac4d317caad80d7add2f09
pcailly <pcailly@pm.me>
2026-04-09 15:04:32 +0200

    feat(cloud/aws): wrap operations behind AwsProvider class

---
+++ src/cloud/aws/provider.ts
@@ -0,0 +1,46 @@
+import { EC2Client } from '@aws-sdk/client-ec2';
+import type {
+  CloudProvider,
+  ImageRef,
+  Instance,
+  InstanceStatus,
+  Location,
+  ProvisionSpec,
+  ResourceLedger,
+} from '../core.js';
+import { resolveNixosAmi } from './images.js';
+import { provisionAws } from './provision.js';
+import { destroyAws } from './destroy.js';
+import { statusAws } from './status.js';
+
+export interface AwsProviderOptions {
+  region: string;
+  profile?: string;
+  imageCacheFile: string;
+}
+
+export class AwsProvider implements CloudProvider {
+  readonly name = 'aws' as const;
+  private readonly ec2: EC2Client;
+
+  constructor(private readonly opts: AwsProviderOptions) {
+    if (opts.profile) process.env.AWS_PROFILE = opts.profile;
+    this.ec2 = new EC2Client({ region: opts.region });
+  }
+
+  async resolveNixosImage(_loc: Location): Promise<ImageRef> {
+    return resolveNixosAmi(this.ec2, this.opts.region, this.opts.imageCacheFile);
+  }
+
+  provision(spec: ProvisionSpec, ledger: ResourceLedger): Promise<Instance> {
+    return provisionAws(this.ec2, spec, ledger);
+  }
+
+  destroy(ledger: ResourceLedger): Promise<void> {
+    return destroyAws(this.ec2, ledger);
+  }
+
+  status(ledger: ResourceLedger): Promise<InstanceStatus> {
+    return statusAws(this.ec2, ledger);
+  }
+}
